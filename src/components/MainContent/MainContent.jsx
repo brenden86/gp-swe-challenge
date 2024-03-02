c1b795fe1bceb87f4ef41e20ea91814236ad47cb
Brenden Koenigsman <brenden.koenigsman@gmail.com>
2024-03-02 16:19:53 -0600

    initialized MainContent component

---
+++ src/components/MainContent/MainContent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AgronomyPage from '../AgronomyPage/AgronomyPage';
+import './MainContent.scss';
+
+export default function MainContent() {
+  return (
+    <div className='main-content'>
+
+      <AgronomyPage />
+
+    </div>
+  )
+}
