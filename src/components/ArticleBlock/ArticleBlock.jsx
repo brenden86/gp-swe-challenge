2f888bad1795256e852d90aab5dadb430beb1881
Brenden Koenigsman <brenden.koenigsman@gmail.com>
2024-03-03 17:25:28 -0600

    populate article blocks from API

--- src/components/ArticleBlock/ArticleBlock.jsx
+++ src/components/ArticleBlock/ArticleBlock.jsx
@@ -1,31 +1,38 @@
 import React from 'react';
 import './ArticleBlock.scss';
 
-export default function ArticleBlock() {
+export default function ArticleBlock({ article }) {
+
+  console.log(article.categorization.tags)
+
   return (
     <article className='article-block'>
 
       <div className="block-content-wrapper">
 
         <div className="article-image">
-          <img src="https://cdn-assets.greatplainsmfg.com/ag_files/styles/scale_500_wide/public/agronomy_images/small_199767479.jpg" alt="turkey" />
+          <img
+            src={article.article_image?.values[0].images['500px_width_uri'] ?? '/src/assets/images/agronomy_placeholder.jpg'}
+            alt={article.article_image?.values[0].alt}
+          />
         </div>
 
         <div className="article-info">
 
           <div className="article-title">
-            <h2>Article Title</h2>
+            <h2>{article.title}</h2>
           </div>
 
           <div className="article-tags">
-            <button className="tag">tag1</button>
-            <button className="tag">tag2</button>
+            {
+              article.categorization.tags.values.map(tag => (
+                <button className="tag">{tag.name}</button>
+              ))
+            }
           </div>
 
           <div className="article-summary">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum laboriosam ducimus, impedit assumenda inventore nemo at totam? Consectetur consequatur a, neque quas veritatis quam sapiente rerum laudantium architecto, deserunt doloremque?
-            Consequatur magnam expedita porro necessitatibus aliquid provident. Amet inventore quod labore harum fugiat totam distinctio repellendus. Quibusdam accusantium adipisci hic quos iusto, iste dolores assumenda delectus doloribus fuga, necessitatibus rerum.
-            Dolorum cupiditate consequatur ducimus eius, ab ea non facere quae animi incidunt! Sunt rem eveniet fuga, maiores sequi possimus! Cum autem exercitationem ea repellat sapiente illum in eum molestiae cumque?
+            {article.body.summary}
           </div>
 
         </div>
@@ -33,7 +40,7 @@ export default function ArticleBlock() {
         
       </div>
 
-      <button>read more</button>
+      <button>Read more</button>
 
     </article>
   )
